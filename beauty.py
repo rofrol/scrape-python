3347308c301203591babb7b695ca33ac7aa624ab
Roman Frołow <rofrol@gmail.com>
2011-01-22 12:15:58 +0100

    pewne produkty sa tylko w kategorii lev1 a nie ma ich nizej, dlatego pobrac wszystkie produkty z linku wyszukaj a potem dodac kategorie

--- beauty.py
+++ beauty.py
@@ -62,83 +62,75 @@ while len(navs1): navs2[navs1.pop()]={}
 for k,v in navs2.items(): print "%s=%s" % (k,v)
 #"""
 
-navs={}
-liczbaproduktowallstep=0
-
 def navsenter(lev1, lev2):
-    print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-    print 'wchodze do navsenter'
+    #print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    #print 'wchodze do navsenter'
     global cats
     global navs
-    global liczbaproduktowallstep
-    print cats[lev1]['name']+':'+cats[lev1]['lev2'][lev2]
+    global liczbaproduktowlev2allstep
+    global liczbaproduktowlev2all
+    #print cats[lev1]['name']+':'+cats[lev1]['lev2'][lev2]['name']
     elemnastrone=45
     baseurl = 'http://sharg.pl/ajax/items_list.php?c_id='+str(lev2)+'&items_count='+str(elemnastrone)
     soup = getfileofurl(baseurl)
-    #l = len(soup.div('a'))
-    #print 'liczba stron='+str(l)
     s = re.compile(r'Liczba produktów: (?P<word>.+)')
     n = s.search(str(soup))
-    liczbaproduktow=int(n.group('word'))
-    print 'liczbaproduktow='+str(liczbaproduktow)
-    liczbastron=int(ceil(liczbaproduktow/float(elemnastrone)))
-    print 'liczbastron='+str(liczbastron)
-    liczba=0
-    if liczbastron > 0:
-        for i in range(liczbastron):
-            url=baseurl+'&page_index='+str(i+1)
-            print url
-            soup = getfileofurl(url)
-            t = re.compile(r'\[(?P<word>T.+)\]')
-            o = t.findall(str(soup))
-            #print "len(soup.table.findAll('a'))/2="+str(len(soup.table.findAll('a'))/2)
-            #for i in soup.table.findAll('a'):
-                #nav=i['href'].replace('item.php?nav=','')
-            for nav in o:
-                print nav
-                if nav in navs:
-                    print 'cannot add to '+cats[lev1]['name']+' : '+cats[lev1]['lev2'][lev2]
-                    print 'bad!!!!!!!!!!!!! '+nav+' already exists in '
-                    l1=navs[nav]['lev1']
-                    l2=navs[nav]['lev2']
-                    print cats[l1]['name']+' : '+cats[l1]['lev2'][l2]
-                    sys.exit(1)
-                navs[nav]={'lev1':lev1, 'lev2':lev2}
-                liczba+=1
-    else:
-        url=baseurl
+    liczbaproduktowlev2=int(n.group('word'))
+    #print 'liczbaproduktowlev2='+str(liczbaproduktowlev2)
+    liczbaproduktowlev2all+=liczbaproduktowlev2
+    liczbastron=int(ceil(liczbaproduktowlev2/float(elemnastrone)))
+    #print 'liczbastron='+str(liczbastron)
+    liczbaproduktowlev2step=0
+    for i in range(liczbastron):
+        url=baseurl+'&page_index='+str(i+1)
         print url
         soup = getfileofurl(url)
         t = re.compile(r'\[(?P<word>T.+)\]')
         o = t.findall(str(soup))
-        #print "len(soup.table.findAll('a'))/2="+str(len(soup.table.findAll('a'))/2)
-        #for i in soup.table.findAll('a'):
-            #nav=i['href'].replace('item.php?nav=','')
         for nav in o:
-            print nav
-            if nav in navs:
+            if nav == 'T001885':
+                logfile = open('fucked.html', 'w')
+                logfile.write(unescape(str(soup)))
+                logfile.close()
                 print nav
-                print lev1
-                print lev2
-                print 'bad!!!!!!!!!!!!! nav already exists'
                 sys.exit(1)
-            navs[nav]={'lev1':lev1, 'lev2':lev2}
-            liczba+=1
-    liczbaproduktowallstep+=liczba
-    print 'liczba='+str(liczba)
-    print 'liczbaproduktowallstep='+str(liczbaproduktowallstep)
-    if liczba == liczbaproduktow:
-        print 'ok '+str(liczba)+'='+str(liczbaproduktow)
-    else:
-        print 'bad!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! '+str(liczba/2)+'!='+str(liczbaproduktow)
+            if nav in navs:
+                #print 'dupl ^^^ here'
+                if lev1 in navs[nav]['cats']:
+                    navs[nav]['cats'][lev1].add(lev2)
+                else:
+                    navs[nav]['cats'][lev1]=set()
+                    navs[nav]['cats'][lev1].add(lev2)
+            else:
+                navs[nav]={}
+                navs[nav]['cats']={}
+                navs[nav]['cats'][lev1]=set()
+                navs[nav]['cats'][lev1].add(lev2)
+            liczbaproduktowlev2step+=1
+    liczbaproduktowlev2allstep+=liczbaproduktowlev2step
+    """
+    print 'liczbaproduktowlev2='+str(liczbaproduktowlev2)
+    print 'liczbaproduktowlev2step='+str(liczbaproduktowlev2step)
+    print 'liczbaproduktowlev2all='+str(liczbaproduktowlev2all)
+    print 'liczbaproduktowlev2allstep='+str(liczbaproduktowlev2allstep)
+    #"""
+    if not liczbaproduktowlev2step == liczbaproduktowlev2:
+        print 'bad!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! '+str(liczbaproduktowlev2step)+'!='+str(liczbaproduktowlev2)
         sys.exit(1)
 
+fname='dupl.txt'
+import os
+if os.path.isfile(fname): os.remove(fname)
+
+navs={}
+liczbaproduktowlev2allstep=0
 
 print "\nnow categories"
 url='http://sharg.pl/index.php'
 soup = getfileofurl(url)
 cats={}
-liczbaproduktowall=0
+liczbaproduktowlev1all=0
+liczbaproduktowlev2all=0
 p = re.compile(r'c_id=(?P<word>.+)&')
 for i in soup('div',"menu"):
     m=p.search(i.a['href'])
@@ -152,28 +144,67 @@ for k,v in cats.items():
     soup = getfileofurl(url)
     s = re.compile(r'Liczba produktów: (?P<word>.+)')
     n = s.search(str(soup))
-    liczbaproduktow=int(n.group('word'))
-    liczbaproduktowall+=liczbaproduktow
+    liczbaproduktowlev1=int(n.group('word'))
+    #print "cccccccccccccccccccccccc"
+    #print 'liczbaproduktowlev1='+str(liczbaproduktowlev1)
+    liczbaproduktowlev1all+=liczbaproduktowlev1
     p = re.compile(r'c_id=(?P<word>.+)&')
     for i in soup('div',"menu"):
         if "&nbsp;|&nbsp;" in str(i):
             m=p.search(i.a['href'])
             key=m.group('word')
             value=unescape(i.a.string.strip())
-            cats[k]['lev2'][key]=value
-            #print '\n'+value
+            cats[k]['lev2'][key]={'name':value}
             navsenter(k, key)
-#for k,v in navs.items(): print "%s=%s" % (k,v)
 
-print "wypisuje"
+print "wypisuje cats"
 for k,v in cats.items():
     print str(k)+"="+v['name']
     for l,m in v['lev2'].items():
-        print '\t'+str(l)+'='+str(m)
+        print '\t'+str(l)+'='+m['name']
 
-print 'liczbaproduktowall='+str(liczbaproduktowall)
-print 'liczbaproduktowallstep='+str(liczbaproduktowallstep)
+print "wypisuje navs"
+for k,v in navs.items():
+    print k
+    for i,j in v['cats'].items():
+        print '\t'+cats[i]['name']
+        for x in j:
+            print '\t\t'+cats[i]['lev2'][x]['name']
+
+print 'liczbaproduktowlev1all='+str(liczbaproduktowlev1all)
+print 'liczbaproduktowlev2all='+str(liczbaproduktowlev2all)
+print 'liczbaproduktowlev2allstep='+str(liczbaproduktowlev2allstep)
 print 'len(navs)='+str(len(navs))
+
+navsall={}
+elemnastrone=45
+baseurl = 'http://sharg.pl/ajax/items_list.php?items_count='+str(elemnastrone)
+soup = getfileofurl(baseurl)
+s = re.compile(r'Liczba produktów: (?P<word>.+)')
+n = s.search(str(soup))
+liczbaproduktowall=int(n.group('word'))
+print 'liczbaproduktowall='+str(liczbaproduktowall)
+liczbastron=int(ceil(liczbaproduktowall/float(elemnastrone)))
+print 'liczbastron='+str(liczbastron)
+liczbaproduktowallstep=0
+for i in range(liczbastron):
+    url=baseurl+'&page_index='+str(i+1)
+    print url
+    soup = getfileofurl(url)
+    t = re.compile(r'\[(?P<word>T.+)\]')
+    o = t.findall(str(soup))
+    for nav in o:
+        navsall[nav]={}
+        liczbaproduktowallstep+=1
+
+print 'liczbaproduktowall='+str(liczbaproduktowlev2all)
+print 'liczbaproduktowallstep='+str(liczbaproduktowlev2allstep)
+print 'len(navsall)='+str(len(navsall))
+
+for key in navsall.keys():
+    if not key in navs:
+            print key
+
 """
 logfile = open('categories.txt', 'w')
 for k,v in cats.items(): logfile.write(("%s=%s" % (k,v))+'\n')
@@ -196,6 +227,7 @@ logfile.close()
 for k,v in lev2.items(): print "%s=%s" % (k,v)
 #"""
 #TODO
+#all products 1597 http://sharg.pl/ajax/items_list.php?m_id=0&search=&x=63&y=5
 #unique list or just set
 #http://www.peterbe.com/plog/uniqifiers-benchmark
 #docs.python.org/release/2.5.2/lib/types-set.html
